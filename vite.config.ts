0942f04315c23d616683c1deaefabc2aa2c1f9eb
Nnaemeka Okalla <nokalla@wyze-ng.com>
2025-03-27 15:18:40 +0100

    test V0

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
       registerType: 'autoUpdate',
       workbox: {
         cleanupOutdatedCaches: true,
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5MB limit
       },
     }),
   ],
